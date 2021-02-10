a5b1954cde358c9414b4b28dde93014c1d9ba0a5
Naresh Kumar H <naresh.20201012@gmail.com>
2021-02-10 12:37:59 +0530

    Updated

---
+++ dist/cjs/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.API = void 0;
+const api_js_1 = require("./api.js");
+Object.defineProperty(exports, "API", { enumerable: true, get: function () { return api_js_1.API; } });
---
+++ dist/esm/api.js
@@ -0,0 +1,3 @@
+export const API = () => {
+    return "Welcome to API";
+};
