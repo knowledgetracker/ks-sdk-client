a623a4818eec36b77ec2b0b4d512dbb048ec0551
Naresh Kumar H <nareshkumarh@live.com>
2021-03-06 07:15:14 +0530

    Added course client api

--- dist/esm/course.client.js
+++ dist/esm/course.client.js
@@ -1,4 +1,14 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import { API_ENVIRONMENT } from "./config";
+import axios from "axios";
 export class CourseClient {
     constructor(config = {}) {
         this.headers = config === null || config === void 0 ? void 0 : config.headers;
@@ -6,7 +16,277 @@ export class CourseClient {
         this.API_URL =
             config.environment === "DEV" ? API_ENVIRONMENT.DEV : API_ENVIRONMENT.PROD;
     }
-    listCourses() {
-        return ["Java"];
+    list() {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = `${this.API_URL}v1/courses`;
+            let response = yield axios.get(url, { headers: this.headers });
+            return response.data;
+        });
+    }
+    findOne(id) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = `${this.API_URL}v1/courses/${id}`;
+            let response = yield axios.get(url, { headers: this.headers });
+            return response.data;
+        });
+    }
+    listModules(courseId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = `${this.API_URL}v1/courses/${courseId}/modules`;
+            let response = yield axios.get(url, { headers: this.headers });
+            return response.data;
+        });
+    }
+    listSections(courseId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = `${this.API_URL}v1/courses/${courseId}/sections`;
+            let response = yield axios.get(url, { headers: this.headers });
+            return response.data;
+        });
+    }
+    save(course) {
+        return __awaiter(this, void 0, void 0, function* () {
+            //let createdBy = this.authService.getLoggedInUserId();
+            //course["createdBy"] = createdBy;
+            let url = `${this.API_URL}v1/courses`;
+            let response = yield axios.post(url, course, { headers: this.headers });
+            return response.data;
+        });
+    }
+    update(id, course) {
+        return __awaiter(this, void 0, void 0, function* () {
+            //let createdBy = this.authService.getLoggedInUserId();
+            //course["createdBy"] = createdBy;
+            let url = `${this.API_URL}v1/courses/${id}`;
+            let response = yield axios.put(url, course, { headers: this.headers });
+            return response.data;
+        });
+    }
+    delete(id) {
+        return __awaiter(this, void 0, void 0, function* () {
+            //let createdBy = this.authService.getLoggedInUserId();
+            let url = `${this.API_URL}v1/courses/${id}`;
+            let response = yield axios.delete(url, { headers: this.headers });
+            return response.data;
+        });
+    }
+    addModule(courseId, module) {
+        return __awaiter(this, void 0, void 0, function* () {
+            // module["createdBy"] = this.authService.getLoggedInUserId();
+            let url = `${this.API_URL}v1/courses/${courseId}/modules`;
+            let response = yield axios.post(url, module, { headers: this.headers });
+            return response.data;
+        });
+    }
+    addSection(courseId, section) {
+        return __awaiter(this, void 0, void 0, function* () {
+            //section["createdBy"] = this.authService.getLoggedInUserId();
+            let url = `${this.API_URL}v1/courses/${courseId}/sections`;
+            let response = yield axios.post(url, section, { headers: this.headers });
+            return response.data;
+        });
+    }
+    addLecture(courseId, lecture) {
+        return __awaiter(this, void 0, void 0, function* () {
+            //lecture["createdBy"] = this.authService.getLoggedInUserId();
+            let url = `${this.API_URL}v1/courses/${courseId}/sections/${lecture.sectionId}/lectures`;
+            //return this.http.post(url, lecture, { headers: this.getHeaders() });
+            let response = yield axios.post(url, lecture, { headers: this.headers });
+            return response.data;
+        });
+    }
+    deleteLecture(courseId, lectureId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            //lecture["createdBy"] = this.authService.getLoggedInUserId();
+            let url = `${this.API_URL}v1/courses/${courseId}/lectures/${lectureId}`;
+            //return this.http.delete(url, { headers: this.getHeaders() });
+            let response = yield axios.delete(url, { headers: this.headers });
+            return response.data;
+        });
+    }
+    deleteModule(courseId, moduleId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = `${this.API_URL}v1/courses/${courseId}/modules/${moduleId}`;
+            //return this.http.delete(url, { headers: this.getHeaders() });
+            let response = yield axios.delete(url, { headers: this.headers });
+            return response.data;
+        });
+    }
+    deleteSection(courseId, sectionId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = `${this.API_URL}v1/courses/${courseId}/sections/${sectionId}`;
+            //return this.http.delete(url, { headers: this.getHeaders() });
+            let response = yield axios.delete(url, { headers: this.headers });
+            return response.data;
+        });
+    }
+    addTopic(courseId, moduleId, topic) {
+        return __awaiter(this, void 0, void 0, function* () {
+            //topic["createdBy"] = this.authService.getLoggedInUserId();
+            let url = `${this.API_URL}v1/courses/${courseId}/modules/${moduleId}/topics`;
+            //return this.http.post(url, topic, { headers: this.getHeaders() });
+            let response = yield axios.post(url, topic, { headers: this.headers });
+            return response.data;
+        });
+    }
+    deleteTopic(courseId, moduleId, topicId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = `${this.API_URL}v1/courses/${courseId}/modules/${moduleId}/topics/${topicId}`;
+            // return this.http.delete(url, { headers: this.getHeaders() });
+            let response = yield axios.delete(url, { headers: this.headers });
+            return response.data;
+        });
+    }
+    listTopics(courseId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = `${this.API_URL}v1/courses/${courseId}/topics`;
+            let response = yield axios.get(url, { headers: this.headers });
+            return response.data;
+        });
+    }
+    getTopics(courseId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = `${this.API_URL}v1/courses/${courseId}/content`;
+            let response = yield axios.get(url, { headers: this.headers });
+            return response.data;
+        });
+    }
+    //@TODO:
+    download(courseId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = `${this.API_URL}v1/courseexport/${courseId}`;
+            // return this.http.get(url, {
+            //   headers: this.getHeaders(),
+            //   responseType: "blob" as "json",
+            // });
+            let response = yield axios.get(url, { headers: this.headers });
+            return response.data;
+        });
+    }
+    publish(courseId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = `${this.API_URL}v1/courses/${courseId}/publish`;
+            // return this.http.put(url, null, { headers: this.getHeaders() });
+            let response = yield axios.put(url, null, { headers: this.headers });
+            return response.data;
+        });
+    }
+    importCourses(file) {
+        return __awaiter(this, void 0, void 0, function* () {
+            //let userId = this.authService.getLoggedInUserId();
+            let userId = "naresh";
+            let url = this.API_URL + `v1/courses/import?createdBy=${userId}`;
+            //return this.http.post(url, file, { headers: this.getHeaders() });
+            let response = yield axios.post(url, file, { headers: this.headers });
+            return response.data;
+        });
+    }
+    uploadCourseImage(courseId, file) {
+        return __awaiter(this, void 0, void 0, function* () {
+            //let userId = this.authService.getLoggedInUserId();
+            let url = this.API_URL + `v1/courses/${courseId}/uploadImage`;
+            let response = yield axios.post(url, file, { headers: this.headers });
+            return response.data;
+        });
+    }
+    getCourseContents(courseId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = `${this.API_URL}v1/courses/${courseId}/contents`;
+            //return this.http.get(url, { headers: this.getHeaders() });
+            let response = yield axios.get(url, { headers: this.headers });
+            return response.data;
+        });
+    }
+    getCourseModuleContents(courseId, moduleId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = `${this.API_URL}v1/courses/${courseId}/lectures/${moduleId}`;
+            //return this.http.get(url, { headers: this.getHeaders() });
+            let response = yield axios.get(url, { headers: this.headers });
+            return response.data;
+        });
+    }
+    getCourseContent(courseId, contentId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = `${this.API_URL}v1/courses/${courseId}/contents/${contentId}`;
+            // return this.http.get(url, { headers: this.getHeaders() });
+            let response = yield axios.get(url, { headers: this.headers });
+            return response.data;
+        });
+    }
+    addCourseContent(content) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = `${this.API_URL}v1/contents`;
+            //return this.http.post(url, content, { headers: this.getHeaders() });
+            let response = yield axios.post(url, content, { headers: this.headers });
+            return response.data;
+        });
+    }
+    updateContent(content) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = `${this.API_URL}v1/contents/${content.id}`;
+            //return this.http.put(url, content, { headers: this.getHeaders() });
+            let response = yield axios.put(url, content, { headers: this.headers });
+            return response.data;
+        });
+    }
+    deleteCourseContent(contentId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = `${this.API_URL}v1/contents/${contentId}`;
+            //return this.http.delete(url, { headers: this.getHeaders() });
+            let response = yield axios.delete(url, { headers: this.headers });
+            return response.data;
+        });
+    }
+    importCourseContent(id, content) {
+        return __awaiter(this, void 0, void 0, function* () {
+            //let userId = this.authService.getLoggedInUserId();
+            let userId = "naresh";
+            let url = this.API_URL + `v1/courses/${id}/import/content?createdBy=${userId}`;
+            // return this.http.post(url, content, { headers: this.getHeaders() });
+            let response = yield axios.post(url, content, { headers: this.headers });
+            return response.data;
+        });
+    }
+    getCourseQuestions(courseId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = `${this.API_URL}v1/courses/${courseId}/questions`;
+            // return this.http.get(url, { headers: this.getHeaders() });
+            let response = yield axios.get(url, { headers: this.headers });
+            return response.data;
+        });
+    }
+    getCourseQuestion(courseId, contentId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = `${this.API_URL}v1/courses/${courseId}/questions/${contentId}`;
+            //return this.http.get(url, { headers: this.getHeaders() });
+            let response = yield axios.get(url, { headers: this.headers });
+            return response.data;
+        });
+    }
+    addCourseQuestion(courseId, content) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = `${this.API_URL}v1/courses/${courseId}/questions`;
+            // return this.http.post(url, content, { headers: this.getHeaders() });
+            let response = yield axios.post(url, content, { headers: this.headers });
+            return response.data;
+        });
+    }
+    deleteCourseQuestion(courseId, contentId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let url = `${this.API_URL}v1/courses/${courseId}/questions/${contentId}`;
+            // return this.http.delete(url, { headers: this.getHeaders() });
+            let response = yield axios.delete(url, { headers: this.headers });
+            return response.data;
+        });
+    }
+    importCourseQuestion(id, content) {
+        return __awaiter(this, void 0, void 0, function* () {
+            //let userId = this.authService.getLoggedInUserId();
+            let userId = "naresh";
+            let url = this.API_URL + `v1/courses/${id}/import/questions?createdBy=${userId}`;
+            // return this.http.post(url, content, { headers: this.getHeaders() });
+            let response = yield axios.post(url, content, { headers: this.headers });
+            return response.data;
+        });
     }
 }
--- src/course.client.ts
+++ src/course.client.ts
@@ -1,5 +1,5 @@
 import { API_ENVIRONMENT } from "./config";
-
+import axios from "axios";
 export class CourseClient {
   headers: any;
   API_URL: string;
@@ -11,7 +11,245 @@ export class CourseClient {
       config.environment === "DEV" ? API_ENVIRONMENT.DEV : API_ENVIRONMENT.PROD;
   }
 
-  listCourses() {
-    return ["Java"];
+  async list() {
+    let url = `${this.API_URL}v1/courses`;
+    let response = await axios.get(url, { headers: this.headers });
+    return response.data;
+  }
+
+  async findOne(id: any) {
+    let url = `${this.API_URL}v1/courses/${id}`;
+    let response = await axios.get(url, { headers: this.headers });
+    return response.data;
+  }
+
+  async listModules(courseId: any) {
+    let url = `${this.API_URL}v1/courses/${courseId}/modules`;
+    let response = await axios.get(url, { headers: this.headers });
+    return response.data;
+  }
+
+  async listSections(courseId: any) {
+    let url = `${this.API_URL}v1/courses/${courseId}/sections`;
+    let response = await axios.get(url, { headers: this.headers });
+    return response.data;
+  }
+
+  async save(course: any) {
+    //let createdBy = this.authService.getLoggedInUserId();
+    //course["createdBy"] = createdBy;
+    let url = `${this.API_URL}v1/courses`;
+    let response = await axios.post(url, course, { headers: this.headers });
+    return response.data;
+  }
+
+  async update(id: any, course: any) {
+    //let createdBy = this.authService.getLoggedInUserId();
+    //course["createdBy"] = createdBy;
+    let url = `${this.API_URL}v1/courses/${id}`;
+    let response = await axios.put(url, course, { headers: this.headers });
+    return response.data;
+  }
+
+  async delete(id: any) {
+    //let createdBy = this.authService.getLoggedInUserId();
+    let url = `${this.API_URL}v1/courses/${id}`;
+    let response = await axios.delete(url, { headers: this.headers });
+    return response.data;
+  }
+
+  async addModule(courseId: any, module: any) {
+    // module["createdBy"] = this.authService.getLoggedInUserId();
+    let url = `${this.API_URL}v1/courses/${courseId}/modules`;
+    let response = await axios.post(url, module, { headers: this.headers });
+    return response.data;
+  }
+
+  async addSection(courseId: any, section: any) {
+    //section["createdBy"] = this.authService.getLoggedInUserId();
+    let url = `${this.API_URL}v1/courses/${courseId}/sections`;
+    let response = await axios.post(url, section, { headers: this.headers });
+    return response.data;
+  }
+
+  async addLecture(courseId: any, lecture: any) {
+    //lecture["createdBy"] = this.authService.getLoggedInUserId();
+    let url = `${this.API_URL}v1/courses/${courseId}/sections/${lecture.sectionId}/lectures`;
+    //return this.http.post(url, lecture, { headers: this.getHeaders() });
+    let response = await axios.post(url, lecture, { headers: this.headers });
+    return response.data;
+  }
+
+  async deleteLecture(courseId: any, lectureId: any) {
+    //lecture["createdBy"] = this.authService.getLoggedInUserId();
+    let url = `${this.API_URL}v1/courses/${courseId}/lectures/${lectureId}`;
+    //return this.http.delete(url, { headers: this.getHeaders() });
+    let response = await axios.delete(url, { headers: this.headers });
+    return response.data;
+  }
+
+  async deleteModule(courseId: any, moduleId: any) {
+    let url = `${this.API_URL}v1/courses/${courseId}/modules/${moduleId}`;
+    //return this.http.delete(url, { headers: this.getHeaders() });
+    let response = await axios.delete(url, { headers: this.headers });
+    return response.data;
+  }
+
+  async deleteSection(courseId: any, sectionId: any) {
+    let url = `${this.API_URL}v1/courses/${courseId}/sections/${sectionId}`;
+    //return this.http.delete(url, { headers: this.getHeaders() });
+    let response = await axios.delete(url, { headers: this.headers });
+    return response.data;
+  }
+
+  async addTopic(courseId: any, moduleId: any, topic: any) {
+    //topic["createdBy"] = this.authService.getLoggedInUserId();
+    let url = `${this.API_URL}v1/courses/${courseId}/modules/${moduleId}/topics`;
+    //return this.http.post(url, topic, { headers: this.getHeaders() });
+    let response = await axios.post(url, topic, { headers: this.headers });
+    return response.data;
+  }
+
+  async deleteTopic(courseId: any, moduleId: any, topicId: any) {
+    let url = `${this.API_URL}v1/courses/${courseId}/modules/${moduleId}/topics/${topicId}`;
+    // return this.http.delete(url, { headers: this.getHeaders() });
+    let response = await axios.delete(url, { headers: this.headers });
+    return response.data;
+  }
+
+  async listTopics(courseId: any) {
+    let url = `${this.API_URL}v1/courses/${courseId}/topics`;
+    let response = await axios.get(url, { headers: this.headers });
+    return response.data;
+  }
+
+  async getTopics(courseId: any) {
+    let url = `${this.API_URL}v1/courses/${courseId}/content`;
+    let response = await axios.get(url, { headers: this.headers });
+    return response.data;
+  }
+
+  //@TODO:
+  async download(courseId: any) {
+    let url = `${this.API_URL}v1/courseexport/${courseId}`;
+    // return this.http.get(url, {
+    //   headers: this.getHeaders(),
+    //   responseType: "blob" as "json",
+    // });
+    let response = await axios.get(url, { headers: this.headers });
+    return response.data;
+  }
+
+  async publish(courseId: any) {
+    let url = `${this.API_URL}v1/courses/${courseId}/publish`;
+    // return this.http.put(url, null, { headers: this.getHeaders() });
+    let response = await axios.put(url, null, { headers: this.headers });
+    return response.data;
+  }
+
+  async importCourses(file: any) {
+    //let userId = this.authService.getLoggedInUserId();
+    let userId = "naresh";
+    let url = this.API_URL + `v1/courses/import?createdBy=${userId}`;
+    //return this.http.post(url, file, { headers: this.getHeaders() });
+    let response = await axios.post(url, file, { headers: this.headers });
+    return response.data;
+  }
+
+  async uploadCourseImage(courseId: any, file: any) {
+    //let userId = this.authService.getLoggedInUserId();
+    let url = this.API_URL + `v1/courses/${courseId}/uploadImage`;
+    let response = await axios.post(url, file, { headers: this.headers });
+    return response.data;
+  }
+
+  async getCourseContents(courseId: any) {
+    let url = `${this.API_URL}v1/courses/${courseId}/contents`;
+    //return this.http.get(url, { headers: this.getHeaders() });
+    let response = await axios.get(url, { headers: this.headers });
+    return response.data;
+  }
+
+  async getCourseModuleContents(courseId: any, moduleId: any) {
+    let url = `${this.API_URL}v1/courses/${courseId}/lectures/${moduleId}`;
+    //return this.http.get(url, { headers: this.getHeaders() });
+    let response = await axios.get(url, { headers: this.headers });
+    return response.data;
+  }
+
+  async getCourseContent(courseId: any, contentId: any) {
+    let url = `${this.API_URL}v1/courses/${courseId}/contents/${contentId}`;
+    // return this.http.get(url, { headers: this.getHeaders() });
+    let response = await axios.get(url, { headers: this.headers });
+    return response.data;
+  }
+
+  async addCourseContent(content: any) {
+    let url = `${this.API_URL}v1/contents`;
+    //return this.http.post(url, content, { headers: this.getHeaders() });
+    let response = await axios.post(url, content, { headers: this.headers });
+    return response.data;
+  }
+
+  async updateContent(content: any) {
+    let url = `${this.API_URL}v1/contents/${content.id}`;
+    //return this.http.put(url, content, { headers: this.getHeaders() });
+    let response = await axios.put(url, content, { headers: this.headers });
+    return response.data;
+  }
+
+  async deleteCourseContent(contentId: any) {
+    let url = `${this.API_URL}v1/contents/${contentId}`;
+    //return this.http.delete(url, { headers: this.getHeaders() });
+    let response = await axios.delete(url, { headers: this.headers });
+    return response.data;
+  }
+
+  async importCourseContent(id: any, content: any) {
+    //let userId = this.authService.getLoggedInUserId();
+    let userId = "naresh";
+    let url =
+      this.API_URL + `v1/courses/${id}/import/content?createdBy=${userId}`;
+    // return this.http.post(url, content, { headers: this.getHeaders() });
+    let response = await axios.post(url, content, { headers: this.headers });
+    return response.data;
+  }
+
+  async getCourseQuestions(courseId: any) {
+    let url = `${this.API_URL}v1/courses/${courseId}/questions`;
+    // return this.http.get(url, { headers: this.getHeaders() });
+    let response = await axios.get(url, { headers: this.headers });
+    return response.data;
+  }
+
+  async getCourseQuestion(courseId: any, contentId: any) {
+    let url = `${this.API_URL}v1/courses/${courseId}/questions/${contentId}`;
+    //return this.http.get(url, { headers: this.getHeaders() });
+    let response = await axios.get(url, { headers: this.headers });
+    return response.data;
+  }
+
+  async addCourseQuestion(courseId: any, content: any) {
+    let url = `${this.API_URL}v1/courses/${courseId}/questions`;
+    // return this.http.post(url, content, { headers: this.getHeaders() });
+    let response = await axios.post(url, content, { headers: this.headers });
+    return response.data;
+  }
+
+  async deleteCourseQuestion(courseId: any, contentId: any) {
+    let url = `${this.API_URL}v1/courses/${courseId}/questions/${contentId}`;
+    // return this.http.delete(url, { headers: this.getHeaders() });
+    let response = await axios.delete(url, { headers: this.headers });
+    return response.data;
+  }
+
+  async importCourseQuestion(id: any, content: any) {
+    //let userId = this.authService.getLoggedInUserId();
+    let userId = "naresh";
+    let url =
+      this.API_URL + `v1/courses/${id}/import/questions?createdBy=${userId}`;
+    // return this.http.post(url, content, { headers: this.getHeaders() });
+    let response = await axios.post(url, content, { headers: this.headers });
+    return response.data;
   }
 }
