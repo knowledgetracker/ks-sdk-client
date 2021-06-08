272b1c2216f9fbc25f97bb9a0e9e18bce36f5a62
Naresh Kumar H <nareshkumarh@live.com>
2021-06-08 23:40:54 +0530

    Updated rest endpoint

--- dist/cjs/config.js
+++ dist/cjs/config.js
@@ -3,9 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.PROJECT_API_URL = exports.API_ENVIRONMENT = void 0;
 exports.API_ENVIRONMENT = {
     DEV: "http://localhost:8080/api/",
-    PROD: "https://knowledgetracker-java-api.herokuapp.com/api/",
+    PROD: "https://api-knowledgetracker-in-3czko.ondigitalocean.app/api/",
 };
 exports.PROJECT_API_URL = {
     DEV: "http://localhost:8080/api/",
-    PROD: "https://projecttracker-api.herokuapp.com/api/",
+    PROD: "https://api.projecttracker.in/api/",
 };
